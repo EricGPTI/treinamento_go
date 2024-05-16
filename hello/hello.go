0f7121d932ddd84db4b5878f8fae8cdb951ff824
EricGPTI <ericgpti@gmail.com>
2024-05-16 18:11:28 -0300

    Chamando codigo do modulo greetings

--- hello/hello.go
+++ hello/hello.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 import "rsc.io/quote"
+import "example.com/greetings"
 
+//Criando primeira função.
 func main() {
 	fmt.Println(quote.Go())
+	fmt.Println(GetGreetings())
 }
+
+//Chamando message em greetings.
+func GetGreetings() string {
+	message := greetings.Hello("Eric")
+	return message
+	//fmt.Println(message)
+}
+
+
