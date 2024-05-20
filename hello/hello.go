bde728b9a668bcb7e4e84301e6af39d5f21937cc
EricGPTI <ericgpti@gmail.com>
2024-05-20 00:06:13 -0300

    Ajustes de funções para receber e retornar msg map e testes

--- hello/hello.go
+++ hello/hello.go
@@ -1,20 +1,30 @@
 package main
 
 import "fmt"
-import "rsc.io/quote"
+//import "rsc.io/quote"
 import "example.com/greetings"
+import "log"
+
 
 //Criando primeira função.
 func main() {
-	fmt.Println(quote.Go())
-	fmt.Println(GetGreetings())
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	//Request a greeting message.
+	message, err := greetings.Hellos(names)
+
+	//If an error was returned, print it to the console and
+	//exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	//If no error was returned, print the returned message
+	//to the console.
+	for _, msg := range message {
+		fmt.Println(msg)
+	}
 }
-
-//Chamando message em greetings.
-func GetGreetings() string {
-	message := greetings.Hello("Eric")
-	return message
-	//fmt.Println(message)
-}
-
-
