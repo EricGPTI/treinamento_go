39a254652f8382da4aa47bd18710e4d4c2b4b21a
EricGPTI <ericgpti@gmail.com>
2024-05-26 15:33:39 -0300

    trabalhando com  multi-module how to

--- hello/hello.go
+++ hello/hello.go
@@ -1,30 +1,10 @@
 package main
 
-import "fmt"
-//import "rsc.io/quote"
-import "example.com/greetings"
-import "log"
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
 
-
-//Criando primeira função.
 func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	// A slice of names
-	names := []string{"Gladys", "Samantha", "Darrin"}
-
-	//Request a greeting message.
-	message, err := greetings.Hellos(names)
-
-	//If an error was returned, print it to the console and
-	//exit the program.
-	if err != nil {
-		log.Fatal(err)
-	}
-	//If no error was returned, print the returned message
-	//to the console.
-	for _, msg := range message {
-		fmt.Println(msg)
-	}
+	fmt.Println(reverse.String("Hello"), reverse.Int(24601))
 }
